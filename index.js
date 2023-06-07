181b37dc62ac0aeb5153101dc3d8491e8d04ff29
Elijah <ecapers721@gmail.com>
2023-06-06 20:54:31 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Elijah Capers",
+    streetAddress: "73 Sterling Street"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const employees = { ...employee};
+    employees[key] = value;
+    return employees;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const employees = { ...employee};
+    delete employees[key];
+    return employees;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
